7c8ae0387f0a28b64d9add77f1ad59c77363e2c8
Carlos Rony <carlosrecla03@gmail.com>
2021-08-25 15:02:57 -0300

    finalização da tarefa.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace contagemregreciva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("É agora!");
+            Console.Write("Você esta pronto ? :");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+             Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Em suas Marcas!");
+            
+            Console.BackgroundColor = ConsoleColor.
+            Yellow;
+             Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Preparar !");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("GO!");
+            Console.WriteLine("GO!");
+              Console.WriteLine("GO!");
+              Console.ResetColor();
+        }
+    }
+}
